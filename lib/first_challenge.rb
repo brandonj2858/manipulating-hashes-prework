ab90b43ebc905079b4254a3b3a7be45ec6f88e8f
Brandon James <brandonj2858@gmail.com>
2019-07-12 21:49:07 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -17,7 +17,7 @@ def first_challenge
   contacts.each do |person, data|
     data.each do |attribute, value|
       if attribute == :favorite_icecream_flavors
-        value.each delete_if
+        value.each
       end
   end
 end
